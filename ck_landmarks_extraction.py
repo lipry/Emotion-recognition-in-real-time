5e65121c237dd9e2a47addffa16a789cb32cdfd4
Lipry <fabiolipreri@me.com>
2016-06-23 13:54:37 +0200

    Implemented get_training_data function

--- ck_landmarks_extraction.py
+++ ck_landmarks_extraction.py
@@ -3,28 +3,34 @@ import sys
 import numpy as np
 import cv2
 import dlib
-
-def normalize_point(point, tl, size):
-    return np.absolute((point - tl).astype('float')/size.astype('float'))
+import face_landmarks
 
 def get_training_data(image_path, labels_path):
-    landmarks_predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+    landmarks_predictor = face_landmarks.faceLandmarks("shape_predictor_68_face_landmarks.dat")
     detector = dlib.get_frontal_face_detector()
 
+    features = []
+    labels = []
     for dirpath, dirnames, filenames in os.walk(image_path):
         #print dirpath
         files = [name for name in glob.glob(dirpath+"/*.png")]
-        if(len(files) != 0):
-            img = cv2.imread(max(files),cv2.IMREAD_GRAYSCALE)
-            dets = detector(img, 1)
-            for k, d in enumerate(dets):
-                normalized_landmarks = np.array([[p.x, p.y] for p in landmarks_predictor(img, d).parts()])
-                for l in landmarks:
-                    cv2.circle(img, (l[0], l[1]), 3, (255,0,0))
-            cv2.imshow('image',img)
-            cv2.waitKey(0)
-            cv2.destroyAllWindows()
 
+        if(len(files) != 0): #if images exist
+            file_path = max(files)
+            pure_name = os.path.splitext(os.path.basename(file_path))[0]
+            emotion = glob.glob(labels_path+"/*/*/"+pure_name+"_emotion.txt")
+            if(len(emotion) > 0): #data about emotion exist
+                in_file = open(emotion[0],"r")
+                label = int(float(in_file.read()))
+                in_file.close()
+                labels.append(label)
+                img = cv2.imread(file_path,cv2.IMREAD_GRAYSCALE)
+                dets = detector(img, 1)
+                #in base case will be iterate only one time (every image in CK+ dataset have one face)
+                for k, rect in enumerate(dets):
+                    landmarks = landmarks_predictor.get_landmarks(img, rect)
+                    features.append(landmarks)
+    return features, labels
 
 if(len(sys.argv) != 3):
     print "Wrong arguments"
